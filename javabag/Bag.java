c24a1642cd4de26281a3e83b2962693c7c3169cb
JoshFigCSUG <joshua.figueroa@csuglobal.edu>
2024-10-17 19:00:22 -0600

    Update Bag.java

--- javabag/Bag.java
+++ javabag/Bag.java
@@ -1,3 +1,40 @@
-public class Bag {
-    
+
+import java.util.ArrayList;
+
+public class Bag<T> {
+    private ArrayList<T> items;
+    public Bag() {
+        items = new ArrayList<>();
+    }
+
+    public void add(T item) {
+        items.add(item);
+    }
+
+    public void remove(T item) {
+        items.remove(item);
+    }
+
+    public boolean contains(T item) {
+        return items.contains(item);
+    }
+
+    public int count(T item) {
+        int count = 0;
+        for (T i : items) {
+            if (i.equals(item)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return items.toString();
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
